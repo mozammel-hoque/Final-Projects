2b83383cf97c8269e191f165fed9b3a339cda4ad
Mozammel Hoque <mh.rimon20@gmail.com>
2021-11-13 10:46:24 -0600

    new

---
+++ Linkedin/src/main/java/mainpage/HomePage.java
@@ -0,0 +1,11 @@
+package mainpage;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+
+    @FindBy (id ="")
+    WebElement logo;
+
+}
