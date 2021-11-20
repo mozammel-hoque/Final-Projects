8deec1ba7384bc185a5dcfe0d7e91b96d25df9ab
Mozammel Hoque <mh.rimon20@gmail.com>
2021-11-19 20:04:36 -0600

    test

---
+++ StateFarm/src/test/java/test/TestInvestment.java
@@ -0,0 +1,20 @@
+package test;
+
+import base.CommonAPI;
+import homepage.HomePage;
+import homepage.Investment;
+import org.junit.Test;
+import org.openqa.selenium.support.PageFactory;
+
+public class TestInvestment extends CommonAPI {
+   // HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+
+//    @Test
+//    public void retirementPlanningFunctionality() throws InterruptedException {
+//        Investment investment = PageFactory.initElements(driver,Investment.class);
+//        investment.getInvestmentButton().click();
+//        waitFor(1);
+//        investment.getRetirementPlanning().click();
+//        waitFor(1);
+//    }
+}
